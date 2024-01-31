b90397176328d75d9de4f1d5bb0f687969f5502b
Davide <DavideBuglione@hotmail.com>
2024-01-31 16:47:05 +0100

    test

---
+++ eserciziow1d3/Program.cs
@@ -0,0 +1,140 @@
+﻿namespace eserciziow1d3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continua = true;
+            ContoCorrente utente = new ContoCorrente();
+
+            while (continua)
+            {
+                Console.WriteLine("Menu ATM");
+                Console.WriteLine("1. Crea Conto");
+                Console.WriteLine("2. Visualizza Saldo");
+                Console.WriteLine("3. Prelevamento");
+                Console.WriteLine("4. Deposita");
+                Console.WriteLine("5. Esci");
+
+                Console.WriteLine("Scegli una opzione (1-5)");
+
+                string scelta = Console.ReadLine();
+                Console.Clear();
+
+                switch (scelta)
+                {
+                    case "1":
+                        Console.Write("Apertura conto:\n- Nome: ");
+                        string name = Console.ReadLine();
+                        Console.Write("- Cognome: ");
+                        string cognome = Console.ReadLine();
+
+                        utente.Name = name;
+                        utente.Surname = cognome;
+                        utente.Account();
+
+                        Console.WriteLine(utente.IsOpen ? "Il conto è stato creato con successo!" : "Il conto è già aperto.");
+                        break;
+
+                    case "2":
+                        Console.WriteLine(utente.CheckBalance());
+                        break;
+
+                    case "3":
+                        Console.Write("Quanto desideri prelevare? ");
+                        if (int.TryParse(Console.ReadLine(), out int withdrawAmount))
+                        {
+                            Console.WriteLine(utente.Withdraw(withdrawAmount));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Inserisci un importo valido.");
+                        }
+                        break;
+
+                    case "4":
+                        Console.Write("Quanto desideri depositare? ");
+                        if (int.TryParse(Console.ReadLine(), out int depositAmount))
+                        {
+                            Console.WriteLine(utente.Deposit(depositAmount));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Inserisci un importo valido.");
+                        }
+                        break;
+
+                    case "5":
+                        continua = false;
+                        Console.WriteLine("Grazie per aver utilizzato il nostro servizio.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Scelta non valida. Riprova.");
+                        break;
+                }
+            }
+        }
+    }
+
+    class ContoCorrente
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        private int Balance { get; set; }
+        public bool IsOpen { get; private set; }
+
+        public string CheckBalance()
+        {
+            return IsOpen ? $"Il tuo saldo attuale è {Balance}" : "Non esiste nessun conto.";
+        }
+
+        public void Account()
+        {
+            if (!IsOpen)
+            {
+                IsOpen = true;
+            }
+        }
+
+        public string Withdraw(int amount)
+        {
+            if (IsOpen)
+            {
+                if (Balance >= amount)
+                {
+                    Balance -= amount;
+                    return $"Il tuo prelievo di {amount} è andato a buon fine.";
+                }
+                else
+                {
+                    return "Non hai abbastanza fondi sul conto.";
+                }
+            }
+            else
+            {
+                return "Non esiste nessun conto.";
+            }
+        }
+
+        public string Deposit(int amount)
+        {
+            if (IsOpen)
+            {
+                if (amount >= 1000)
+                {
+                    Balance += amount;
+                    return "Il deposito è andato a buon fine!";
+                }
+                else
+                {
+                    return "Devi depositare almeno 1000.";
+                }
+            }
+            else
+            {
+                return "Non esiste nessun conto.";
+            }
+        }
+    }
+}
---
+++ eserciziow1d3p2/Program.cs
@@ -0,0 +1,77 @@
+﻿
+    //internal class program
+    //{
+    //    static void main(string[] args)
+    //    {
+    //        string[] myarr = new string[3];
+
+    //        string nome1 = console.readline();
+    //        string nome2 = console.readline();
+    //        string nome3 = console.readline();
+
+    //        myarr[0] = nome1;
+    //        myarr[1] = nome2;
+    //        myarr[2] = nome3;
+
+    //        console.writeline(myarr[0]);
+    //        console.writeline(myarr[1]);
+    //        console.writeline(myarr[2]);
+
+
+
+    //        console.writeline("inserisci un nome da cercare:");
+    //        string nomedacercare = console.readline();
+
+    //        bool nometrovato = cercacerca(myarr, nomedacercare);
+
+    //        if (nometrovato)
+    //        {
+    //            console.writeline($"il nome '{nomedacercare}' è presente nell'array.");
+    //        }
+    //        else
+    //        {
+    //            console.writeline($"il nome '{nomedacercare}' non è presente nell'array.");
+    //        }
+
+    //    }
+
+    //    public static bool cercacerca(string[] myarr, string nomedacercare)
+    //    {
+    //        foreach (string nome in myarr)
+    //        {
+    //            if (nome == nomedacercare)
+    //            {
+    //                return true;
+    //            }
+    //        }
+    //        return false;
+    //    }
+    //}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scegli la dimenzione dell'array");
+            int lunghezzaArray =int.Parse( Console.ReadLine());
+            int[] myArr = new int[ lunghezzaArray ];
+
+            for(int i = 0; i < lunghezzaArray; i++)
+            {
+                Console.WriteLine("Scrivi un numero");
+                myArr[i] = int.Parse(Console.ReadLine());  
+            }
+
+            int somma = 0;
+
+            for(int i = 0;i < lunghezzaArray;i++)
+            {
+                somma += myArr[i];
+            }
+
+            Console.WriteLine($"la somma è {somma}");
+
+            Console.WriteLine($"la media aritmetica è {somma / lunghezzaArray}");
+        }
+    }
+
